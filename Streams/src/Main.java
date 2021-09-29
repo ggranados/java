195e58b630014cad964bc5ba5959dcb5623547b9
Guillermo Granados <guillermo.granados@outlook.com>
2021-09-29 17:51:13 -0300

    Added imperative approach

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -1,10 +1,22 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Person> people = getPeople();
-        
+
+        // Imperative approach
+
+        List<Person> females = new ArrayList<>();
+
+        for(Person person: people){
+            if(person.getGender().equals(Gender.FEMALE)){
+                females.add(person);
+            }
+        }
+
+        females.forEach(System.out::println);
     }
 
 
