1de3ee5e6d2f9cfe5507e3e4f6dc22cba72640c7
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-04 14:56:25 -0300

    Added simplest Runnable implementation

---
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/CalculateAverage.java
@@ -0,0 +1,9 @@
+package cert.java11.ocp.chapter18.concurrency;
+
+public class CalculateAverage implements Runnable{
+
+    @Override
+    public void run() {
+        // Define work here that uses the scores object
+    }
+}
