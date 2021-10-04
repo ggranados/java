553ae9f300f900e5002ae3f1fb16895c589189b3
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-04 17:09:51 -0300

    Added passed information to Runnable

--- OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/CalculateAverage.java
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/CalculateAverage.java
@@ -2,6 +2,12 @@ package cert.java11.ocp.chapter18.concurrency;
 
 public class CalculateAverage implements Runnable{
 
+    private double[] scores;
+
+    public CalculateAverage(double[] scores){
+        this.scores = scores;
+    }
+
     @Override
     public void run() {
         // Define work here that uses the scores object
