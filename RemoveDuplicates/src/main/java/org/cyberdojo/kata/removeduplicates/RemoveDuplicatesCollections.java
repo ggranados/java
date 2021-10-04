7732fb1d782b4556e2dc965fc6887c414bac5fc3
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-04 19:04:15 -0300

    Added Java collections implementation

---
+++ RemoveDuplicates/src/main/java/org/cyberdojo/kata/removeduplicates/RemoveDuplicatesCollections.java
@@ -0,0 +1,24 @@
+package org.cyberdojo.kata.removeduplicates;
+
+import java.util.*;
+
+public class RemoveDuplicatesCollections {
+
+    public static String[] removeDuplicates(String args[]) {
+
+        // Empty & Null case
+        if(args == null || args.length == 0)
+            return new String[0];
+
+        // Minimal case
+        if(args.length == 1)
+            return args;
+
+        // Other cases
+
+        SortedSet set = new TreeSet(Comparator.naturalOrder());
+        Collections.addAll(set, args);
+
+        return (String[]) set.toArray(new String[0]);
+    }
+}
