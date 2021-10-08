b5e0d5d337982e4f03698660db4d98678a158bce
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-08 18:28:29 -0300

    Added grouped by then flat map

--- Streams/src/org/java17/streams/advanced/AdvancedStreamSamples.java
+++ Streams/src/org/java17/streams/advanced/AdvancedStreamSamples.java
@@ -2,6 +2,8 @@ package org.java17.streams.advanced;
 
 import org.java17.streams.pojo.Person;
 import org.java17.streams.pojo.User;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Locale;
 import java.util.TreeMap;
@@ -36,16 +38,16 @@ public class AdvancedStreamSamples {
         //mapped.stream().forEach(System.out::println);
 
         // Map to unmodifiable after group by
-        var groupedByThenMap = getPeople()
+        var groupedByThenUnmodifiable = getPeople()
                 .stream()
                 .collect(
                         Collectors.collectingAndThen(
                                 Collectors.groupingBy(Person::getGender),
-                                c -> Collections.unmodifiableMap(c)
+                                c -> Collections.unmodifiableMap(c) // Collections::unmodifiableList
                         )
                 );
 
-        //System.out.println(groupedByThenMap);
+        //System.out.println(groupedByThenUnmodifiable);
 
         //group by then tree map of sets
         var groupedByThenTreeMap = getPeople()
@@ -53,7 +55,32 @@ public class AdvancedStreamSamples {
                 .collect(
                         Collectors.groupingBy(Person::getGender, TreeMap::new, Collectors.toSet())
                 );
-        System.out.println(groupedByThenTreeMap);
+        //System.out.println(groupedByThenTreeMap);
+
+        //Grouped by then map to new pojo
+        var groupedByThenMap = getPeople()
+                .stream()
+                .collect(
+                    Collectors.groupingBy(Person::getGender,
+                            Collectors.mapping(p -> {
+                                return new User(p.getName(),p.getName());
+                            }, Collectors.toList())
+                    )
+                );
+        //System.out.println(groupedByThenMap);
+
+        //Grouped by then flat map (is there a way to avoid double stream??)
+        var groupedByThenFlatMap = getPeople()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(Person::getGender,
+                                Collectors.mapping(p -> {
+                                    return new User(p.getName(),p.getName());
+                                }, Collectors.toList())
+                        )
+                ).entrySet().stream().flatMap(e -> e.getValue().stream())
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(groupedByThenFlatMap);
     }
 
 
