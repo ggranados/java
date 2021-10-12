f918fa4bd897fecd7cbed3d536dcc1e6f46d467f
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-12 18:24:15 -0300

    Added try lock segment

--- OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/threadsafety/lock/TryLockSample.java
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/threadsafety/lock/TryLockSample.java
@@ -3,11 +3,21 @@ package cert.java11.ocp.chapter18.concurrency.threadsafety.lock;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Main {
+public class TryLockSample {
     public static void main(String[] args) {
 
         Lock lock = new ReentrantLock();
         new Thread(() -> printMessage(lock)).start();
+
+        if(lock.tryLock()) {
+            try {
+                System.out.println("Lock obtained, entering protected code");
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println("Unable to acquire lock, doing something else");
+        }
     }
 
     public static void printMessage(Lock lock) {
