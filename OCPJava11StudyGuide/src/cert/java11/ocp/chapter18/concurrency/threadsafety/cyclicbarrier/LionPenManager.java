a25f831dad4233a33589d7b0d90efd8ebe6eb0ee
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-12 19:50:08 -0300

    Changed to coordinate access with cyclic barrier implementation

--- OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/threadsafety/cyclicbarrier/LionPenManager.java
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/threadsafety/cyclicbarrier/LionPenManager.java
@@ -5,18 +5,28 @@ public class LionPenManager {
     private void removeLions() {System.out.println("Removing lions");}
     private void cleanPen() {System.out.println("Cleaning the pen");}
     private void addLions() {System.out.println("Adding lions");}
-    public void performTask() {
+    public void performTask(CyclicBarrier c1, CyclicBarrier c2) {
         removeLions();
-        cleanPen();
-        addLions();
+        try {
+            c1.await();
+            cleanPen();
+            c2.await();
+            addLions();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
         ExecutorService service = null;
         try {
             service = Executors.newFixedThreadPool(4);
             var manager = new LionPenManager();
+            var c1 = new CyclicBarrier(4);
+            var c2 = new CyclicBarrier(4,
+                    () -> System.out.println("*** Pen Cleaned!"));
+
             for (int i = 0; i < 4; i++)
-                service.submit(() -> manager.performTask());
+                service.submit(() -> manager.performTask(c1,c2));
         } finally {
             if (service != null) service.shutdown();
         }
