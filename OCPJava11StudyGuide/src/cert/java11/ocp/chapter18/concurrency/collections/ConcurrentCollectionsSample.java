aed2a2b0bf5756f3435605aeda96a8d9cdf8c644
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-12 20:08:54 -0300

    Fixed concurrent modification of collection

--- OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/collections/ConcurrentCollectionsSample.java
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/collections/ConcurrentCollectionsSample.java
@@ -1,10 +1,10 @@
 package cert.java11.ocp.chapter18.concurrency.collections;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentCollectionsSample {
     public static void main(String[] args) {
-        var foodData = new HashMap<String, Integer>();
+        var foodData = new ConcurrentHashMap<String, Integer>();
         foodData.put("penguin", 1);
         foodData.put("flamingo", 2);
         for(String key: foodData.keySet())
