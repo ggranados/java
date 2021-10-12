ec5027e97e56e5b93d8249701806621bdeae437b
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-12 17:36:15 -0300

    Added sinchronized r/w segment

--- OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/threadsafety/SheepManager.java
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/threadsafety/SheepManager.java
@@ -4,18 +4,17 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SheepManager {
-
-    private AtomicInteger sheepCount = new AtomicInteger(0);
-
+    private int sheepCount = 0;
     private void incrementAndReport() {
-        System.out.print(sheepCount.incrementAndGet()+" ");
+        synchronized(this) {
+            System.out.print((++sheepCount)+" ");
+        }
     }
-
     public static void main(String[] args) {
         ExecutorService service = null;
         try {
             service = Executors.newFixedThreadPool(20);
-            SheepManager manager = new SheepManager();
+            var manager = new SheepManager();
             for(int i = 0; i < 10; i++)
                 service.submit(() -> manager.incrementAndReport());
         } finally {
