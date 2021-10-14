3a95a93a6d30d0c8e183865873b49b14a498af3a
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-14 19:10:14 -0300

    Added concurrent map and linked queue sample

--- OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/collections/ConcurrentCollectionsSample.java
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/collections/ConcurrentCollectionsSample.java
@@ -1,13 +1,26 @@
 package cert.java11.ocp.chapter18.concurrency.collections;
 
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class ConcurrentCollectionsSample {
+public class    ConcurrentCollectionsSample {
     public static void main(String[] args) {
         var foodData = new ConcurrentHashMap<String, Integer>();
         foodData.put("penguin", 1);
         foodData.put("flamingo", 2);
         for(String key: foodData.keySet())
             foodData.remove(key);
+
+        Map<String,Integer> map = new ConcurrentHashMap<>();
+        map.put("zebra", 52);
+        map.put("elephant", 10);
+        System.out.println(map.get("elephant"));  // 10
+
+        Queue<Integer> queue = new ConcurrentLinkedQueue<>();
+        queue.offer(31);
+        System.out.println(queue.peek());  // 31
+        System.out.println(queue.poll());  // 31
     }
 }
