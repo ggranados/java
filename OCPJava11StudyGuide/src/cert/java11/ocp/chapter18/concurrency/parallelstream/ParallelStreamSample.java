1b43910861abe428972edbf081f182b614b193b6
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-15 18:44:23 -0300

    Created non-concurrent stream processing taking 25 secs

---
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/parallelstream/ParallelStreamSample.java
@@ -0,0 +1,26 @@
+package cert.java11.ocp.chapter18.concurrency.parallelstream;
+
+import java.util.List;
+
+public class ParallelStreamSample {
+
+    private static int doWork(int input) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {}
+        return input;
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        List.of(1,2,3,4,5)
+                .stream()
+                .map(w -> doWork(w))
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        var timeTaken = (System.currentTimeMillis()-start)/1000;
+        System.out.println("Time: "+timeTaken+" seconds");
+    }
+
+}
