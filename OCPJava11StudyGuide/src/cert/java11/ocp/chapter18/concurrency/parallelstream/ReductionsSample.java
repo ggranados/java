0a24db1677d884c9a34f7f6ba8d2361ea3e2acfb
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-18 17:26:29 -0300

    Added parallel stream and reduction

--- OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/parallelstream/ReductionsSample.java
+++ OCPJava11StudyGuide/src/cert/java11/ocp/chapter18/concurrency/parallelstream/ReductionsSample.java
@@ -3,7 +3,9 @@ package cert.java11.ocp.chapter18.concurrency.parallelstream;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ReductionsSample {
@@ -41,5 +43,20 @@ public class ReductionsSample {
                 Set::add,
                 Set::addAll);
         System.out.println(set);  // [f, l, o, w]
+
+        // parallel stream and reduction
+        Stream<String> ohMy = Stream.of("lions","tigers","bears").parallel();
+        ConcurrentMap<Integer, String> map = ohMy
+                .collect(Collectors.toConcurrentMap(String::length,
+                        k -> k,
+                        (s1, s2) -> s1 + "," + s2));
+        System.out.println(map); // {5=lions,bears, 6=tigers}
+        System.out.println(map.getClass());  // java.util.concurrent.ConcurrentHashMap
+
+        var ohMy2 = Stream.of("lions","tigers","bears").parallel();
+        ConcurrentMap<Integer, List<String>> map2 = ohMy2
+                .collect(Collectors.groupingByConcurrent(String::length));
+        System.out.println(map2); // {5=[lions, bears], 6=[tigers]}
+        System.out.println(map2.getClass());
     }
 }
