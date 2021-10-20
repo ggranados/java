3c7dc92ec69ad77cc9b70d99912e1a92e567fd21
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-20 19:29:10 -0300

    Added hashmap compose null supported but lightly executed

--- FunctionalProgramming/src/org/linkedinlearning/functionalprogramming/fis/FunctionalInterfaces.java
+++ FunctionalProgramming/src/org/linkedinlearning/functionalprogramming/fis/FunctionalInterfaces.java
@@ -6,13 +6,33 @@ import java.util.Objects;
 
 public class FunctionalInterfaces {
     public static void main(String[] args) {
-        System.out.println(composeHashCodes("Hello","world"));
-        System.out.println(composeHashCodes("Hello",null));
+        System.out.println(composeHashCodes3("Hello","world"));
+        System.out.println(composeHashCodes3("Hello",null));
     }
 
     private static int composeHashCodes(Object a, Object b) {
         return a.hashCode() ^ b.hashCode();
     }
 
-  
+    // executes getApplicationStatus each time a var is evaluated, even if not null
+    private static int composeHashCodes2(Object a, Object b) {
+        Objects.requireNonNull(a, "a may be not null!" + getApplicationStatus());
+        Objects.requireNonNull(b, "b may be not null!" + getApplicationStatus());
+
+        return a.hashCode() ^ b.hashCode();
+    }
+
+    private static int composeHashCodes3(Object a, Object b) {
+        Objects.requireNonNull(a, () -> "a may be not null!" + getApplicationStatus());
+        Objects.requireNonNull(b, () -> "b may be not null!" + getApplicationStatus());
+
+        return a.hashCode() ^ b.hashCode();
+    }
+
+
+    private static String getApplicationStatus() {
+        System.out.println("getApplicationStatus");
+        return "It's " + LocalTime.now();
+    }
+
 }
