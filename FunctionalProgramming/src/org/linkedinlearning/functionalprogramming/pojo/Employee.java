68ea8b23e94e1f6339aad5599436e248e8beb82d
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-20 20:17:12 -0300

    Added comparator compose example

--- FunctionalProgramming/src/org/linkedinlearning/functionalprogramming/pojo/Employee.java
+++ FunctionalProgramming/src/org/linkedinlearning/functionalprogramming/pojo/Employee.java
@@ -24,5 +24,27 @@ public class Employee {
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
+    public static Employee[] getData(){
+        Employee dept[] = new Employee[8];
+        dept[0] = new Employee("Alec", 1500);
+        dept[1] = new Employee("Bob", 1600);
+        dept[2] = new Employee("Claire", 1700);
+        dept[3] = new Employee("Danielle", 1800);
+        dept[4] = new Employee("Ethan", 1900);
+        dept[5] = new Employee("Bob", 1000);
+        dept[6] = new Employee("Alex", 1900);
+        dept[7] = new Employee("Bob", 1900);
+
+        return dept;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
 
