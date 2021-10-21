315ed16737d77a04c11ba6deff126e19739e3a57
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-21 14:40:57 -0300

    Created simple book pojo

---
+++ FunctionalProgramming/src/org/linkedinlearning/functionalprogramming/pojo/Book.java
@@ -0,0 +1,16 @@
+package org.linkedinlearning.functionalprogramming.pojo;
+
+import java.util.List;
+
+public class Book {
+
+    private String title;
+    private  List<String> words;
+
+    public Book(String title, List<String> words) {
+        this.title = title;
+        this.words = words;
+    }
+
+
+}
