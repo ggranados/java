4c97cd6be7d29139407062b53fbce5c5860f6a0e
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-21 15:44:05 -0300

    Get an immutable list of words

--- FunctionalProgramming/src/org/linkedinlearning/functionalprogramming/pojo/Book.java
+++ FunctionalProgramming/src/org/linkedinlearning/functionalprogramming/pojo/Book.java
@@ -1,6 +1,10 @@
 package org.linkedinlearning.functionalprogramming.pojo;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Book {
 
@@ -12,9 +16,35 @@ public class Book {
         this.words = words;
     }
 
+    public List<String> getWords() {
+        return Collections.unmodifiableList(words);
+    }
+
     static Book newBook(String title, List<String> words){
         return new Book(title,words);
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", words=" + words +
+                '}';
+    }
+
+    public static Set<Book> getData(){
+        var books =
+                Stream.iterate(0, i-> i+1)
+                        .limit(5)
+                        .map( i -> new Book("Book No."+i,List.of("uno","dos","tres")))
+                        .peek(System.out::println)
+                        .collect(Collectors.toSet());
+
+        return books;
+    }
+
+    public static void main(String[] args) {
+        Book.getData();
+    }
 
 }
