b50df230177b25e52c4978cdc05789d6c174f753
Guillermo Granados <guillermo.granados@outlook.com>
2021-11-09 12:41:50 -0300

    added first sample for optionals

---
+++ Streams/src/org/java17/amigoscode/optionals/Main.java
@@ -0,0 +1,14 @@
+package org.java17.amigoscode.optionals;
+
+import java.util.Optional;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var value = Optional.ofNullable("Hello")
+                .orElseGet(() -> "default");
+
+        System.out.println(value);
+
+    }
+}
