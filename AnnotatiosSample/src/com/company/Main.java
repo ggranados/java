e69962c9d5a89960c63f0d2b1810a963e72b9667
Guillermo Granados <guillermo.granados@outlook.com>
2022-01-23 19:13:45 -0300

    Added marker annotation to class, field and method

--- AnnotatiosSample/src/com/company/Main.java
+++ AnnotatiosSample/src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+@EmptyAnnotation
 public class Main {
 
+    @EmptyAnnotation
     private static final int THING = 3;
-    
+
+    @EmptyAnnotation
     public static void main(String[] args) {
 	// write your code here
     }
