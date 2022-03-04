ef185dab1d97f8420953be9c55ae8050326698dc
Guillermo Granados <guillermo.granados@outlook.com>
2022-03-03 21:50:01 -0300

    Added procedural dto mapping

--- FunctionalProgramming/src/org/youtube/functionalprogramming/patterns/UserFacade.java
+++ FunctionalProgramming/src/org/youtube/functionalprogramming/patterns/UserFacade.java
@@ -1,2 +1,28 @@
-package org.youtube.functionalprogramming.patterns;public class UserFacade {
+package org.youtube.functionalprogramming.patterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserFacade {
+
+    private UserRepo userRepo;
+
+    public List<UserDto> getAllUser(){
+        List<User> users = userRepo.findAll();
+        List<UserDto> dtos = new ArrayList<>();
+        for(User user : users){
+            UserDto dto = toDto(user);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    private UserDto toDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setUsername(user.getUserName());
+        dto.setFullName(user.getFirstName() + " " + user.getLastName());
+        dto.setActive(user.getDeactivationDate == null);
+        return dto;
+    }
+
 }
