028e9aa5b1a4603b68f4a3c2b17f2e5a6bda73ae
Guillermo Granados <guillermo.granados@outlook.com>
2022-03-09 19:25:02 -0300

    Added fix for returned null with if else

--- FunctionalProgramming/src/org/youtube/functionalprogramming/patterns/nullpointer/DiscountService.java
+++ FunctionalProgramming/src/org/youtube/functionalprogramming/patterns/nullpointer/DiscountService.java
@@ -3,7 +3,12 @@ package org.youtube.functionalprogramming.patterns.nullpointer;
 public class DiscountService {
 
     private String getDiscountLine(Customer customer){
-        return  "Discount%: " + getDiscountPercentage(customer.getMemberCard());
+        Integer d = getDiscountPercentage(customer.getMemberCard());
+        if (d != null){
+            return "Discount%: " + d;
+        }
+        else
+            return "";
     }
 
     private Integer getDiscountPercentage(MemberCard card){
@@ -21,5 +26,6 @@ public class DiscountService {
     public static void main(String[] args) {
         DiscountService service = new DiscountService();
         System.out.println(service.getDiscountLine(new Customer(new MemberCard (60))));
+        System.out.println(service.getDiscountLine(new Customer(new MemberCard (10))));
     }
 }
