22f896551d2146f6763b39d346b98345c64d0af0
Guillermo Granados <guillermo.granados@outlook.com>
2022-05-16 01:40:52 -0400

    [InsertSort]added recursive sort class and unit test

--- Algorithms/src/main/java/edu/ggranados/java/algorithms/sort/InsertSort.java
+++ Algorithms/src/main/java/edu/ggranados/java/algorithms/sort/InsertSort.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class InsertSort {
 
+    public static final int ZERO = 0;
+
     public List<Integer> sort(List<Integer> elements){
 
         for(int j = 1; j < elements.size(); j++){
@@ -22,6 +24,36 @@ public class InsertSort {
         }
 
         return elements;
+    }
+
+    public List<Integer> sortRecursive(List<Integer> elements, int currentIndex){
+
+        if( isLowerThan( currentIndex , elements.size()) ) {
+            int currentElement = elements.get(currentIndex);
+            int fromIndex = currentIndex - 1;
+            replaceBackwards(currentElement, elements, fromIndex);
+            return sortRecursive(elements, ++currentIndex);
+        }
+
+        return elements;
+    }
+
+    private boolean isLowerThan( int a, int b) {
+        return a < b;
+    }
+
+    private void replaceBackwards(int currentElement, List<Integer> elements, int fromIndex){
+
+        if(isGreaterOrEqualsThan(fromIndex, ZERO) && isLowerThan(currentElement, elements.get(fromIndex))){
+            elements.set(fromIndex + 1, elements.get(fromIndex));
+            elements.set(fromIndex, currentElement);
+            replaceBackwards(currentElement, elements, --fromIndex);
+        }
+
+    }
+
+    private boolean isGreaterOrEqualsThan(int a, int b) {
+        return a >= b;
     }
 
 
--- Algorithms/src/test/java/edu/ggranados/java/algorithms/sort/InsertSortTest.java
+++ Algorithms/src/test/java/edu/ggranados/java/algorithms/sort/InsertSortTest.java
@@ -24,12 +24,38 @@ class InsertSortTest {
 
 
         //Then
-        sortedElements.stream()
+        var lastElement = sortedElements.stream()
                 .reduce((a,b) -> {
-                assertTrue(isEqualOrGreaterThan(b, a),
-                        b + " element is not equal or greater than previous " + a);
-                  return b;
+                    assertTrue(isEqualOrGreaterThan(b, a),
+                            b + " element is not equal or greater than previous " + a);
+                    return b;
                 });
+        assertEquals(elements.stream().mapToInt(v -> v).max().getAsInt(),
+                lastElement.get(),
+                "Last element should be the max value");
+    }
+
+    @Test
+    void testSortRecursive(){
+        //Given
+        elements.addAll(Arrays.asList(33,2,5,4,67,4,2,1,46,7,34));
+        System.out.println(elements);
+
+        //When
+        var sortedElements = insertSort.sortRecursive(elements,1);
+        System.out.println(sortedElements);
+
+
+        //Then
+        var lastElement = sortedElements.stream()
+                .reduce((a,b) -> {
+                    assertTrue(isEqualOrGreaterThan(b, a),
+                            b + " element is not equal or greater than previous " + a);
+                    return b;
+                });
+        assertEquals(elements.stream().mapToInt(v -> v).max().getAsInt(),
+                lastElement.get(),
+                "Last element should be the max value");
     }
 
 
