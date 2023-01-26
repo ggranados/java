b107971be095a2a82324a627d0132d3bbcf573b5
Guillermo Granados <guillermo.granados@outlook.com>
2023-01-25 22:57:29 -0300

    [streams] refactored factorial with streams

--- Streams/src/org/java17/streams/challenges/FactorialCalculator.java
+++ Streams/src/org/java17/streams/challenges/FactorialCalculator.java
@@ -5,15 +5,19 @@ import java.util.stream.IntStream;
 public class FactorialCalculator {
 
     public static void main(String[] args) {
+        var calculator = new FactorialCalculatorRecursive();
 
         int n = 5;
-        var calculator = new FactorialCalculator();
-
         long result = calculator.apply(n);
         System.out.println(result);
-        assert result == 120 : "incorrect factorial calculation";
+        assert result == 120 : "incorrect factorial calculation for n= " + n;
 
         n = 0;
+        result = calculator.apply(n);
+        System.out.println(result);
+        assert result == 1 : "incorrect factorial calculation for n= " + n;
+
+        n = -1;
         try {
             calculator.apply(n);
         }catch (IllegalArgumentException e){
@@ -22,11 +26,10 @@ public class FactorialCalculator {
         }
 
 
-
     }
 
-    private long apply(int n) {
-        if(n<=0) throw new IllegalArgumentException("Invalid value for n");
+    public long apply(int n) {
+        if(n<0) throw new IllegalArgumentException("Invalid value for n");
 
         return IntStream.iterate(n, i-> i>0, i-> i-1)
                 .reduce(1, (a,b)-> a*b);
