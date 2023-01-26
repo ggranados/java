731e30ad76d0313e74724d53d2bf8c6473e45fd6
Guillermo Granados <guillermo.granados@outlook.com>
2023-01-25 23:03:34 -0300

    [streams] added factorial tail recursive

---
+++ Streams/src/org/java17/streams/challenges/FactorialCalculatorTailRecursive.java
@@ -0,0 +1,38 @@
+package org.java17.streams.challenges;
+
+public class FactorialCalculatorTailRecursive {
+
+    public static void main(String[] args) {
+
+        var calculator = new FactorialCalculatorTailRecursive();
+
+        int n = 5;
+        long result = calculator.apply(n,1);
+        System.out.println(result);
+        assert result == 120 : "incorrect factorial calculation for n= " + n;
+
+        n = 0;
+        result = calculator.apply(n, 1);
+        System.out.println(result);
+        assert result == 1 : "incorrect factorial calculation for n= " + n;
+
+        n = -1;
+        try {
+            calculator.apply(n, 1);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            assert true : "Exception expected";
+        }
+
+    }
+
+    public long apply(int n, long accumulator) {
+        if(n < 0 ) throw new IllegalArgumentException("Invalid value for n=" + n);
+
+        if(n <= 1 ) return accumulator;
+
+        return apply(n-1, accumulator * n);
+    }
+
+
+}
