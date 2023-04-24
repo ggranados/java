eff1dbd83b47aacfc331ade072abd729f2b66116
Guillermo Granados <guillermo.granados@outlook.com>
2023-04-24 19:31:01 -0400

    [algorithms] enhanced getIntegerList

--- Algorithms/src/main/java/edu/ggranados/java/algorithms/utils/IntegerUtils.java
+++ Algorithms/src/main/java/edu/ggranados/java/algorithms/utils/IntegerUtils.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -17,11 +18,15 @@ public final class IntegerUtils {
     private IntegerUtils() {}
 
     public static List<Integer> getIntegerList(int size) {
+        return getIntegerList(size, 1_000_000);
+    }
+
+    public static List<Integer> getIntegerList(int size, int bound){
         Random random;
         try {
             random = SecureRandom.getInstanceStrong();
-            IntStream randStream = random.ints(size);
-            return randStream.boxed().toList();
+            IntStream randStream = random.ints(size, Integer.MIN_VALUE, bound);
+            return new ArrayList<>(randStream.boxed().toList());
         } catch (NoSuchAlgorithmException e) {
             logger.warn("Could not create secure random");
             return Arrays.asList(1, 5, 2, 3, 4, 6, 7, 5, 2);
